85970127b679cb2e2cd504246370f13c6fdacbc7
Mainak Karmakar <mainakkarmakar.lg@gmail.com>
2025-07-28 12:26:09 +0530

    lms functionality added

--- src/pages/LectureGuide.jsx
+++ src/pages/LectureGuide.jsx
@@ -6,6 +6,13 @@ import {
   filterAllLectures,
 } from "../redux/slices/lectureguide/lectureGuide"; // Import the actions
 import LectureGuideDesktopHeader from "../components/lectureguide/LectureGuideDesktopHeader";
+import { CgNotes } from "react-icons/cg";
+import { CiFolderOn } from "react-icons/ci";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { AiOutlineLike } from "react-icons/ai";
+import { LuFlag } from "react-icons/lu";
+import { BiDislike, BiLike } from "react-icons/bi";
+import { IoIosPlayCircle } from "react-icons/io";
 
 function LectureGuide() {
   const [openedIndices, setOpenedIndices] = useState(new Set());
@@ -62,7 +69,7 @@ function LectureGuide() {
                 >
                   {openedIndices.has(s.id) && (
                     <div
-                      className="bg-green-100 p-2 flex overflow-x-auto w-full scrollbar-thin scrollbar-thumb-gray-400 relative"
+                      className="p-2 flex overflow-x-auto w-full scrollbar-thin scrollbar-thumb-gray-400 relative"
                       onWheel={(e) => {
                         e.stopPropagation();
                         const container = e.currentTarget;
@@ -82,11 +89,59 @@ function LectureGuide() {
                       {s.chapterLectures.map((lecture, index) => (
                         <div
                           key={index}
-                          className="flex-shrink-0 mr-4 min-w-[200px] min-h-[150px] bg-yellow-300"
+                          className="flex-shrink-0 mr-4 min-w-[300px] min-h-[200px] border-2 p-4 rounded-lg shadow-md hover:shadow-lg transition-all"
                         >
-                          <p className="whitespace-nowrap p-2">
-                            {lecture.name}
-                          </p>
+                          <div className="flex justify-between items-center mb-4">
+                            <div className="flex gap-2 justify-center items-center">
+                              <p className="font-semibold text-gray-700">1.</p>
+                              <p className="font-medium text-lg text-gray-800">
+                                Lorem Ipsum
+                              </p>
+                            </div>
+                            <p className="text-sm text-gray-500">
+                              Class Lecture
+                            </p>
+                          </div>
+
+                          <div className="flex flex-col space-y-2">
+                            <div className="flex items-center gap-2">
+                              <CgNotes className="text-blue-500" />
+                              <p className="text-sm text-gray-600">
+                                LOS Covered in this topic
+                              </p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <CiFolderOn className="text-green-500" />
+                              <p className="text-sm text-gray-600">
+                                Content Covered
+                              </p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <HiOutlineLightBulb className="text-yellow-500" />
+                              <p className="text-sm text-gray-600">
+                                Complete This as soon as possible
+                              </p>
+                            </div>
+                          </div>
+                          <div className="mt-6 flex justify-between items-center">
+                            <hr className="border-gray-400 w-full" />
+                            <div className="flex gap-4 text-gray-500">
+                              <LuFlag className="cursor-pointer hover:text-blue-500 transition-all" />
+                              <CgNotes className="cursor-pointer hover:text-blue-500 transition-all" />
+                              <BiLike className="cursor-pointer hover:text-green-500 transition-all" />
+                              <BiDislike className="cursor-pointer hover:text-red-500 transition-all" />
+                            </div>
+
+                            <div className="border-l-2 h-10 mx-4" />
+                            <div className="flex items-center gap-2">
+                              <IoIosPlayCircle className="text-xl text-blue-600" />
+                              <p className="text-sm text-gray-600">10:10:10</p>
+                            </div>
+                            <input
+                              type="checkbox"
+                              className="ml-4 cursor-pointer"
+                            />
+                          </div>
                         </div>
                       ))}
                     </div>
