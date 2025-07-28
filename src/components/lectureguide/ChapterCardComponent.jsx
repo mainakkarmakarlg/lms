c3668f6e220fa534f76a4ad541d55ba6871ed604
Mainak Karmakar <mainakkarmakar.lg@gmail.com>
2025-07-28 12:42:59 +0530

    some changes added

--- src/components/lectureguide/ChapterCardComponent.jsx
+++ src/components/lectureguide/ChapterCardComponent.jsx
@@ -12,24 +12,25 @@ function ChapterCardComponent() {
     <div className="flex-shrink-0 mr-4 min-w-[300px] min-h-[200px] border-2 p-4 rounded-lg shadow-md hover:shadow-lg transition-all">
       <div className="flex justify-between items-center mb-4">
         <div className="flex gap-2 justify-center items-center">
-          <p className="font-semibold text-gray-700">1.</p>
-          <p className="font-medium text-lg text-gray-800">Lorem Ipsum</p>
+          <p className="font-medium text-lg text-[#656D77]">1. Lorem Ipsum</p>
         </div>
-        <p className="text-sm text-gray-500">Class Lecture</p>
+        <div className="border rounded-xl p-1 bg-[#FFF2E4]">
+          <p className="text-[13px] text-[#FFAF5E]">Class Lecture</p>
+        </div>
       </div>
 
       <div className="flex flex-col space-y-2">
-        <div className="flex items-center gap-2">
-          <CgNotes className="" />
-          <p className="text-sm text-gray-600">LOS Covered in this topic</p>
+        <div className="flex items-center gap-2 text-[#8C8F9F]">
+          <CgNotes className="text-[#595b66] text-[17px]" />
+          <p className="text-sm text-[#8C8F9F]">LOS Covered in this topic</p>
         </div>
         <div className="flex items-center gap-2">
-          <CiFolderOn className="" />
-          <p className="text-sm text-gray-600">Content Covered</p>
+          <CiFolderOn className="text-[#595b66] text-[17px]" />
+          <p className="text-sm text-[#8C8F9F]">Content Covered</p>
         </div>
         <div className="flex items-center gap-2">
-          <HiOutlineLightBulb className="" />
-          <p className="text-sm text-gray-600">
+          <HiOutlineLightBulb className="text-[#595b66] text-[17px]" />
+          <p className="text-sm text-[#8C8F9F]">
             Complete This as soon as possible
           </p>
         </div>
@@ -43,7 +44,7 @@ function ChapterCardComponent() {
           <BiDislike className="cursor-pointer hover:text-red-500 transition-all" />
         </div>
 
-        <div className="border-l-2 h-10 mx-4" />
+        <div className="border-l-2 h-5 mx-4" />
         <div className="flex items-center gap-2">
           <IoIosPlayCircle className="text-xl text-red-600" />
           <p className="text-sm text-gray-600">10:10:10</p>
